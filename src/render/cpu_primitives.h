746f25419bf5a61566afefc2b46877ba6ed3acee
7Limes <milesb2288@gmail.com>
2026-05-12 14:43:30 -0400

    Add line clipping

--- src/render/cpu_primitives.h
+++ src/render/cpu_primitives.h
@@ -66,9 +66,89 @@ static inline void surf_draw_rect(SDL_Surface *surf, int x, int y, int width, in
 }
 
 
+// Region codes for Cohen-Sutherland
+#define CS_INSIDE 0
+#define CS_LEFT   1
+#define CS_RIGHT  2
+#define CS_BOTTOM 4
+#define CS_TOP    8
+
+// Helper to compute the region code for a point (x, y)
+static inline int compute_region_code(int x, int y, int w, int h) {
+    int code = CS_INSIDE;
+
+    if (x < 0)           code |= CS_LEFT;
+    else if (x >= w)     code |= CS_RIGHT;
+    if (y < 0)           code |= CS_BOTTOM;
+    else if (y >= h)     code |= CS_TOP;
+
+    return code;
+}
+
+static inline int surf_clip_line(SDL_Surface* surf, int* x1, int* y1, int* x2, int* y2) {
+    int w = surf->w;
+    int h = surf->h;
+    
+    int code1 = compute_region_code(*x1, *y1, w, h);
+    int code2 = compute_region_code(*x2, *y2, w, h);
+    int accept = 0;
+
+    while (true) {
+        if (!(code1 | code2)) {
+            // Both points inside window
+            accept = 1;
+            break;
+        }
+        else if (code1 & code2) {
+            // Both points share an outside region (completely outside)
+            break;
+        }
+        else {
+            // Some segment is inside; calculate intersection
+            int code_out = code1 ? code1 : code2;
+            int x, y;
+
+            // Standard line-intersection formulas
+            if (code_out & CS_TOP) {
+                x = *x1 + (*x2 - *x1) * (h - 1 - *y1) / (*y2 - *y1);
+                y = h - 1;
+            }
+            else if (code_out & CS_BOTTOM) {
+                x = *x1 + (*x2 - *x1) * (0 - *y1) / (*y2 - *y1);
+                y = 0;
+            }
+            else if (code_out & CS_RIGHT) {
+                y = *y1 + (*y2 - *y1) * (w - 1 - *x1) / (*x2 - *x1);
+                x = w - 1;
+            }
+            else if (code_out & CS_LEFT) {
+                y = *y1 + (*y2 - *y1) * (0 - *x1) / (*x2 - *x1);
+                x = 0;
+            }
+
+            // Replace outside point with intersection point
+            if (code_out == code1) {
+                *x1 = x;
+                *y1 = y;
+                code1 = compute_region_code(*x1, *y1, w, h);
+            }
+            else {
+                *x2 = x;
+                *y2 = y;
+                code2 = compute_region_code(*x2, *y2, w, h);
+            }
+        }
+    }
+
+    return accept;
+}
+
 // Draw a line.
-// TODO: line clipping
 static inline void surf_draw_line(SDL_Surface* surf, int x1, int y1, int x2, int y2, Uint32 color) {
+    if (!surf_clip_line(surf, &x1, &y1, &x2, &y2)) {
+        return;
+    }
+
     Uint32 *pixels = (Uint32*) surf->pixels;
     
     int dx = abs(x2 - x1);
@@ -78,9 +158,7 @@ static inline void surf_draw_line(SDL_Surface* surf, int x1, int y1, int x2, int
     int err = dx - dy;
     
     while (1) {
-        if (x1 >= 0 && x1 < surf->w && y1 >= 0 && y1 < surf->h) {
-            pixels[y1 * surf->w + x1] = color;
-        }
+        pixels[y1 * surf->w + x1] = color;
 
         if (x1 == x2 && y1 == y2) {
             break;
